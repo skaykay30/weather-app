22594009ce4edd4064bfc995bae4893bf4229fd2
unknown <80020578+skaykay30@users.noreply.github.com>
2021-03-28 17:33:37 -0500

    Re Save

---
+++ src/index.js
@@ -0,0 +1,54 @@
+
+let now = new Date();
+
+let h2 = document.querySelector("h2");
+let date = now.getDate();
+let days = ["Sun", "Mon", "Tue", "Wed", "Thurs", "Fri", "Sat"];
+let day = days[now.getDay()];
+let months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+let month = months[now.getMonth()];
+let year = now.getFullYear();
+let hours = now.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+
+h2.innerHTML = `${day}, ${month} ${date}, ${year} ${hours}:${minutes}`;
+
+function displayWeatherNow(response);{}
+document.querySelector("#city").innerHTML = response.data.name;
+document.querySelector("#temperature").innerHTML = Math.round(response.data.main.temp);
+document.querySelector("#humidity").innerHTML(response.data.main.humidity);
+document.querySelector("#wind").innerHTML(response.data.wind.speed);
+
+
+
+
+function search(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input").value;
+  let apiKey = "4965d06b98f571e5178fcdd155827c21"
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+  axios.get(apiUrl).then(displayWeatherNow)
+}
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", search);
+
+
